c3ce4dd211c4e7410f76e374142202ffcacf34d1
gao0304 <48554366+GaoFeng0304@users.noreply.github.com>
2020-03-21 19:53:09 +0800

    更新

---
+++ java小练习/平时小练习代码/src/AboutStrictfp.java
@@ -0,0 +1,13 @@
+strictfp class AboutStrictfp {
+    private static double aDouble = 0.05522222222233212d;
+    private static float aFloat = 033311113334410222f;
+
+    public static void main(String[] args) {
+
+        double cDouble = aDouble / aFloat;
+
+        System.out.println("aDouble:" + aDouble);
+        System.out.println("aFloat:" + aFloat);
+        System.out.println("cDouble:" + cDouble);
+    }
+}
